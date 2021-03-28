2339eb4d5906b58776464844ec391529a91cf6cc
Alex Cline <rev.a.r.cline@gmail.com>
2021-03-28 15:44:00 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your functions here
+// mappers
+function map(src, fn) {
+  let out = [];
+
+  for (const val of src) {
+    out.push(fn(val));
+  }
+
+  return out;
+}
+
+// reducers
+function reduce(src, fn, start) {
+  let out = start ? start : src[0];
+  let i = start ? 0 : 1;
+
+  for (; i < src.length; i++) {
+    out = fn(src[i], out);
+  }
+
+  return out;
+}
